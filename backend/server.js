b51bb56782ccc5362937d18423db39a7e69778c1
RomwelFeliciano <romfeliciano18@gmail.com>
2024-07-31 08:13:33 +0800

    Adding Netlify Front End

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://diary-app-cv6f.onrender.com"],
+    origin: ["http://localhost:3000", "https://diary-app-cv6f.onrender.com", "https://diary-appsample.netlify.app"],
   })
 );
 app.use("/api/notes", notesRoute);
